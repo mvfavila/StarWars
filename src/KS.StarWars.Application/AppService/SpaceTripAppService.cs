156e2571b2954e79a61234a25b84c61fe2d0b0c0
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2018-01-11 17:20:46 +1000

    KS.StarWars.Application project added.

---
+++ src/KS.StarWars.Application/AppService/SpaceTripAppService.cs
@@ -0,0 +1,31 @@
+﻿using KS.StarWars.Application.Interfaces;
+using KS.StarWars.Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace KS.StarWars.Application.AppService
+{
+    public class SpaceTripAppService : ISpaceTripAppService
+    {
+        public IEnumerable<Tuple<string, int>> GetAllResuplyStopsForSpaceTrip(SpaceTrip spaceTrip)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // Cleanup
+        }
+
+        ~SpaceTripAppService()
+        {
+            Dispose(false);
+        }
+    }
+}
---
+++ src/KS.StarWars.Application/Interfaces/ISpaceTripAppService.cs
@@ -0,0 +1,11 @@
+﻿using KS.StarWars.Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace KS.StarWars.Application.Interfaces
+{
+    public interface ISpaceTripAppService : IDisposable
+    {
+        IEnumerable<Tuple<string, int>> GetAllResuplyStopsForSpaceTrip(SpaceTrip spaceTrip);
+    }
+}
