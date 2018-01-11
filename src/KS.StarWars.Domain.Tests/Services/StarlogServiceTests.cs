fc10b327a9cd7d8856d0b644298ec839a565beea
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2018-01-12 01:22:58 +1000

    Domain StarlogServiceTests added.

---
+++ src/KS.StarWars.Domain.Tests/Services/StarlogServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using AutoMoq;
+using KS.StarWars.Domain.Interfaces.Repository.ReadOnly;
+using KS.StarWars.Domain.Services;
+using Moq;
+using Xunit;
+
+namespace KS.StarWars.Domain.Tests.Services
+{
+    public class StarlogServiceTests
+    {
+        private readonly AutoMoqer mocker;
+
+        public StarlogServiceTests()
+        {
+            mocker = new AutoMoqer();
+        }
+
+        [Fact(DisplayName = "GetStarshipsByPage must execute repository method once")]
+        [Trait("Starlog", "Domain Service")]
+        public void Starlog_GetStarshipsByPage_MustExecuteRepositoryMethodOnce()
+        {
+            // Arrange
+            mocker.Create<StarlogService>();
+
+            var starlogService = mocker.Resolve<StarlogService>();
+            var starlogPageReadOnlyRepository = mocker.GetMock<IStarlogPageReadOnlyRepository>();
+
+            // Act
+            starlogService.GetStarshipsByPage();
+
+            // Assert
+            starlogPageReadOnlyRepository.Verify(s => s.GetStarshipsByPage(It.IsAny<int>()), Times.Once());
+        }
+    }
+}
